dd3f660786a242ba1e559463fff7a6571718b13a
Rashmitha Kaushalya <151978018+rashmithakau@users.noreply.github.com>
2025-01-09 20:16:11 +0530

    add user service bases url

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
-const { port } = require('./config');
+const { port, userUrl } = require('./config');
 
 app.listen(port, () => {
-    console.log(`BFF is running on http://localhost:${port}`);
+    console.log(`BFF is running on http://localhost:${port} USER_SERVICE_BASE_URL: ${userUrl} PRODUCT_SERVICE_BASE_URL: ${process.env.PRODUCT_SERVICE_BASE_URL}`);
 });
