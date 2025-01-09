9394b4cbfdc47a1c55b00673bda0a029e2420c3e
Rashmitha Kaushalya <151978018+rashmithakau@users.noreply.github.com>
2025-01-09 20:58:34 +0530

    add jwd exp time

--- src/utils/jwtHelper.js
+++ src/utils/jwtHelper.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const {jwtExpiration,jwtSecret} = require('../config');
 
 const generateToken = (payload) => {
-    return jwt.sign(payload, 'default_secret_key',  { expiresIn: jwtExpiration });
+    return jwt.sign(payload, 'default_secret_key',  { expiresIn: '1h' });
 };
 
 const verifyToken = (token) => {
